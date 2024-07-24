b9bbb6024579a1f3636c1a9ab3ea50727a675433
Keerthana Venkatesan <135305964+KeerthanaVenkatesann@users.noreply.github.com>
2024-07-24 08:08:11 +0530

    php on

---
+++ Client.php
@@ -0,0 +1,30 @@
+<?php
+
+// $name = "keerthana";
+// $name .= " Venkatesan";
+
+
+// $age = 25;
+// if($age < 27){
+// $name = "baby";
+// }
+// else{
+//     $name = "keerthana"; 
+// }
+
+// for($i = 0 ; $i <= 10;$i++ ){
+//     echo $i;
+// }
+
+$a=$_POST["a"];
+$b=$_POST["b"];
+$c=$a+$b;
+?>
+<html>
+    <head>
+
+    </head>
+    <body>
+        <h1>my name <?php echo $c; ?>  </h1>
+    </body>
+</html>
---
+++ Index.php
@@ -0,0 +1,10 @@
+<html>
+    <head></head>
+    <body>
+        <form action="Client.php" method="post">
+            <input type="text"name="a">
+            <input type="text" name="b">
+            <button>submit</button>
+        </form>
+    </body>
+</html>
