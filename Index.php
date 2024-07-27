29be64bf4166bee7741b09354a0a7fdb924de714
Keerthana Venkatesan <135305964+KeerthanaVenkatesann@users.noreply.github.com>
2024-07-27 08:12:28 +0530

    loops

--- Index.php
+++ Index.php
@@ -18,21 +18,49 @@
 //     echo $i."<br>";
 //     $i++;
 // }while($i<=5);
-$ary=[8,4,7,2];
-$sum=0;
-for($i=0;$i<count($ary);$i++){
-    $sum +=$ary[$i];
+// $ary=[8,4,7,2,10,5,11];
+// $sum=0;
+// foreach($ary as $value){
+//     if($value % 2 ==1){
+//         $sum +=$value;
+//     }
+
   
+// }
+// echo $sum;
+$ary=[8,4,7,2,10,5,11];
+$sum=0;
+foreach($ary as $key=> $value){
+        $sum +=$key;
 }
 echo $sum;
+$name="KeeRthAna";
+$name2=strlen($name);
+echo $name2;
  ?>
 
 <html>
-    <head></head>
+    <head>
+        <style>
+            table,td{
+                border:1px solid black;
+                border-collapse:collapse;
+                padding:10px 20px;
+            }
+        </style>
+    </head>
     <body>
     <form action="" method="post">
         <input type="number" name="a" value="<?php echo $b; ?>">
         <button>Submit</button>
     </form>
+    <table>
+        <?php foreach($ary as $key=> $value){ ?>
+        <tr>
+            <td> <?php echo $key; ?></td>
+            <td><?php echo $value; ?></td>
+        </tr>
+        <?php } ?>
+    </table>
     </body>
 </html>
